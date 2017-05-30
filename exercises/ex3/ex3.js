03bc78a83ab825829bf642afd5f06e7ef78b7d47
Igor Datsenko <Igor.datsenko@hotmail.com>
2017-05-31 01:09:22 +0300

    exercise 3;

--- exercises/ex3/ex3.js
+++ exercises/ex3/ex3.js
@@ -20,8 +20,29 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+	return new Promise(function(resolve) {
+    fakeAjax(file, resolve);
+  });
 }
+
+var p1 = getFile('file1');
+var p2 = getFile('file2');
+var p3 = getFile('file3');
+
+p1
+		.then(output)
+		.then(function() {
+      console.log('asdf');
+      return p2;
+})
+    .then(output)
+    .then(function() {
+      return p3;
+})
+    .then(output)
+    .then(function() {
+  output('Completed');
+});
 
 // request all files at once in "parallel"
 // ???
